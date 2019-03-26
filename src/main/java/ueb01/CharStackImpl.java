0b5f304bfc27b20a8215361bdd5c0bf80c26ee51
Alexander Gornung <alexander.gornung@web.de>
2019-03-26 12:43:52 +0100

    meine Loesung

--- src/main/java/ueb01/CharStackImpl.java
+++ src/main/java/ueb01/CharStackImpl.java
@@ -1,4 +1,30 @@
 package ueb01;
 
+import java.util.NoSuchElementException;
+
 class CharStackImpl implements CharStack {
+
+    private CharElement top;
+    private int size;
+
+    @Override
+    public void push(char c) {
+        top = new CharElement(c, top);
+        size++;
+    }
+
+    @Override
+    public char pop() {
+        if (top == null)
+            throw new NoSuchElementException();
+        char v = top.value;
+        top = top.next;
+        size--;
+        return v;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
 }
--- src/main/java/ueb01/Palindrom.java
+++ src/main/java/ueb01/Palindrom.java
@@ -4,6 +4,41 @@ class Palindrom {
 	static boolean istPalindrom(String s) {
 		// Verwenden Sie die Klasse CharStackImpl um auf Palindrom zu testen!
 
+		if (s.length() == 1 || s.length() == 0)
+		    return true;
+
+        CharStack cs = new CharStackImpl();
+        char[] chars = s.replaceAll("\\s", "").toLowerCase().toCharArray();
+
+        int  i = 0;
+
+        if (chars.length % 2 == 0) {
+            for (; cs.size() <= chars.length / 2; i++) {
+                cs.push(chars[i]);
+            }
+            for (; cs.size() > 0; i++) {
+                if (cs.pop() == chars[i])
+                    continue;
+
+                return false;
+            }
+        }
+
+        if (chars.length % 2 == 1) {
+            for (; cs.size() <= chars.length / 2; i++) {
+                cs.push(chars[i]);
+            }
+
+            i++;
+
+            for (; cs.size() > 0; i++) {
+                if (cs.pop() == chars[i])
+                    continue;
+
+                return false;
+            }
+        }
+
 		return false;
 	}
 }
